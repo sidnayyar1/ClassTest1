e81e8f282d2f109d1dfba6c242a9df74b754875c
sidharth nayyar <sidnayyar1@gmail.com>
2020-08-20 20:24:21 -0400

    [Sidharth]Added connectivity

--- ClassTest1 WatchKit Extension/InterfaceController.swift
+++ ClassTest1 WatchKit Extension/InterfaceController.swift
@@ -8,9 +8,13 @@
 
 import WatchKit
 import Foundation
+import WatchConnectivity
 
-
-class InterfaceController: WKInterfaceController {
+class InterfaceController: WKInterfaceController,WCSessionDelegate {
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+        
+    }
+    
 
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
